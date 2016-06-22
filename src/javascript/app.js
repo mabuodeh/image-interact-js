7424f7f25125b79127be226f7849da138ce5a402
Muhammed AbuOdeh <mabuodeh3@gmail.com>
2016-06-22 13:55:56 +0100

    Added rectangles to image

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -4,52 +4,99 @@
 
   var interact = require("../vendor/interact-1.2.6.js");
 
-  var pixelSize = 16;
+  var svgCanvas = document.querySelector('svg'),
+      svgNS = 'http://www.w3.org/2000/svg',
+      rectangles = [];
 
-  interact('.rainbow-pixel-canvas')
-    .snap({
-      // snap to the corners of a grid
-      mode: 'grid',
-      // specify the grid dimensions
-      grid: { x: pixelSize, y: pixelSize }
+  var hardwareButton = document.getElementById("hardwareButton");
+
+  // function addElement(tagName) {
+  //     var element = document.createElement(tagName);
+  //     document.appendChild(element);
+  //     return element;
+  // }
+
+  hardwareButton.addEventListener('click', function() {
+    var rect = hardwareButton.getBoundingClientRect();
+     new Rectangle(50, rect.bottom - 100, 40, 40, svgCanvas);
+  }, false);
+    
+  function Rectangle (x, y, w, h, svgCanvas) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.stroke = 5;
+    this.el = document.createElementNS(svgNS, 'rect');
+    
+    this.el.setAttribute('data-index', rectangles.length);
+    this.el.setAttribute('class', 'edit-rectangle');
+    rectangles.push(this);
+
+    this.draw();
+    svgCanvas.appendChild(this.el);
+  }
+
+  Rectangle.prototype.draw = function () {
+    this.el.setAttribute('x', this.x + this.stroke / 2);
+    this.el.setAttribute('y', this.y + this.stroke / 2);
+    this.el.setAttribute('width' , this.w - this.stroke);
+    this.el.setAttribute('height', this.h - this.stroke);
+    this.el.setAttribute('stroke-width', this.stroke);
+  }
+
+  interact('.edit-rectangle')
+    // change how interact gets the
+    // dimensions of '.edit-rectangle' elements
+    .rectChecker(function (element) {
+      // find the Rectangle object that the element belongs to
+      var rectangle = rectangles[element.getAttribute('data-index')];
+
+      // return a suitable object for interact.js
+      return {
+        left  : rectangle.x,
+        top   : rectangle.y,
+        right : rectangle.x + rectangle.w,
+        bottom: rectangle.y + rectangle.h
+      };
     })
-    .origin('self')
+    .inertia({
+      // don't jump to the resume location
+      // https://github.com/taye/interact.js/issues/13
+      zeroResumeDelta: true
+    })
+    .restrict({
+      // restrict to a parent element that matches this CSS selector
+      drag: 'svg',
+      // only restrict before ending the drag
+      endOnly: true,
+      // consider the element's dimensions when restricting
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    })
     .draggable({
       max: Infinity,
-      maxPerElement: Infinity
+      onmove: function (event) {
+        var rectangle = rectangles[event.target.getAttribute('data-index')];
+
+        rectangle.x += event.dx;
+        rectangle.y += event.dy;
+        rectangle.draw();
+      }
     })
-    // draw colored squares on move
-    .on('dragmove', function (event) {
-      var context = event.target.getContext('2d'),
-          // calculate the angle of the drag direction
-          dragAngle = 180 * Math.atan2(event.dx, event.dy) / Math.PI;
+    .resizable({
+      max: Infinity,
+      onmove: function (event) {
+        var rectangle = rectangles[event.target.getAttribute('data-index')];
 
-      // set color based on drag angle and speed
-      context.fillStyle = 'hsl(' + dragAngle + ', 86%, ' +
-       (30 + Math.min(event.speed / 1000, 1) * 50) + '%)';
-
-      // draw squares
-      context.fillRect(event.pageX - pixelSize / 2, event.pageY - pixelSize / 2,
-                       pixelSize, pixelSize);
-    })
-    // clear the canvas on doubletap
-    .on('doubletap', function (event) {
-      var context = event.target.getContext('2d');
-
-      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+        rectangle.w = Math.max(rectangle.w + event.dx, 10);
+        rectangle.h = Math.max(rectangle.h + event.dy, 10);
+        rectangle.draw();
+      }
     });
-
-    function resizeCanvases () {
-      [].forEach.call(document.querySelectorAll('.rainbow-pixel-canvas'), function (canvas) {
-        canvas.width = document.body.clientWidth;
-        canvas.height = window.innerHeight * 0.7;
-      });
-    }
-
-    // interact.js can also add DOM event listeners
-    interact(document).on('DOMContentLoaded', resizeCanvases);
-    interact(window).on('resize', resizeCanvases);
 
   interact.maxInteractions(Infinity);
 
+  // for (var i = 0; i < 5; i++) {
+  //   new Rectangle(50 + 100 * i, 80, 80, 80, svgCanvas);
+  // }
 }());
